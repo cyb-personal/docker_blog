8403e48fc172e1f2b6b9ed9d1646390491fa07f7
cyb-personal <1712066095@qq.com>
2026-05-24 13:35:49 +0800

    Delete blog-forum directory

--- blog-forum/backend/comment-service/src/main/java/com/blog/comment/mapper/PostCountMapper.java
+++
@@ -1,7 +0,0 @@
-package com.blog.comment.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface PostCountMapper {
-    int updateCommentCount(@Param("id") Long id, @Param("count") int count);
-}
--- blog-forum/backend/like-service/src/main/java/com/blog/like/mapper/PostCountMapper.java
+++
@@ -1,7 +0,0 @@
-package com.blog.like.mapper;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface PostCountMapper {
-    int updateLikeCount(@Param("id") Long id, @Param("count") int count);
-}
